6cf8a6d5f2c9bb524b1aa765ffeec176a5ce89fd
Morgan Justine <morgan.justine@me.com>
2024-09-23 17:51:10 +0200

    error pages

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -135,7 +135,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'console': {
-            'level': 'ERROR',
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
         },
         'sentry': {
