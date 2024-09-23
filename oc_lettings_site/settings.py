37799a28d024a6ed34ba471b8c2fb040c8cdc049
Morgan Justine <morgan.justine@me.com>
2024-09-23 23:16:18 +0200

    requirement changes

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 LOGGING = {
