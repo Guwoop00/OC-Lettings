c77c9df94dc18b284fd58a6ece9cd66d693aada2
Morgan Justine <morgan.justine@me.com>
2024-09-24 13:58:18 +0200

    Collect static to Render

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -130,9 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
 
 LOGGING = {
     'version': 1,
